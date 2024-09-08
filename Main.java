6ae65dc928f6d9f9c4b31c605c33f4006e556b3b
Faizan-Comp <sweeptheblade@gmail.com>
2024-09-08 16:44:11 -0400

    Custom Exceptions
    Learning Custom Exceptions in java.

--- Main.java
+++ Main.java
@@ -1,35 +1,11 @@
-import java.io.*;
-
 public class Main {
-    public static void main(String[] args) {
-
-        String[] names = {"John", "Carl", "Jerry"};
-        try {
-            BufferedWriter wr = new BufferedWriter(new FileWriter("output.txt"));
-            wr.write("Writing to a file");
-            wr.append("\nNice try buddy.");
-
-            for(String s : names) {
-                wr.write("\n" + s);
-            }
-            wr.close();
-        }
-        catch(IOException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            BufferedReader br = new BufferedReader(new FileReader("output.txt"));
-            String line;
-            while((line = br.readLine()) != null) {
-                System.out.println(line);
-            }
-            br.close();
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    public static void main(String[] args) throws AgeLessThanZeroException {
+        validateAge(-3);
     }
 
+    private static void validateAge (int age) throws AgeLessThanZeroException {
+        if(age < 0) {
+            throw new AgeLessThanZeroException("Age cannot be negative.", new RuntimeException());
+        }
+    }
 }
