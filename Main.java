2f8c99135d9886734a6d3b93061cb05b01e7210d
Faizan-Comp <sweeptheblade@gmail.com>
2024-09-08 16:41:51 -0400

    Learning Collections
    Learning Collections.

--- Main.java
+++ Main.java
@@ -1,41 +1,44 @@
-enum Planet{
-    MERCURY(1, 1282820),
-    VENUS(2, 282828),
-    EARTH(3, 122),
-    MARS(4, 22999),
-    JUPITER(5, 282.2902),
-    SATURN(6, 289282),
-    URANUS(7, 22882),
-    NEPTUNE(8, 77777),
-    PLUTO(9, 99999);
-
-    int number;
-    double age;
-    Planet(int number, double age){
-        this.number = number;
-        this.age = age;
-    }
-}
+import java.lang.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
+        /*
+        The most simple and common type of list, an Array List.
+         */
+         ArrayList<String> people = new ArrayList<String>();
 
-        Planet myPlanet = Planet.EARTH;
-        Live(myPlanet);
-    }
+         people.add("John");
+         people.add("Dave");
+         people.add("Jack");
 
-    static void Live(Planet myPlanet) {
-        switch(myPlanet) {
-            case EARTH:
-                System.out.println("You can live here.");
-                System.out.println("This is planet number " + myPlanet.number +
-                        " with an age of " + myPlanet.age);
-            break;
-            default:
-                System.out.println("You can't live here.");
-                System.out.println("This is planet number " + myPlanet.number +
-                        " with an age of " + myPlanet.age);
-            break;
-        }
+         people.remove("Dave");
+         people.remove(1);
+
+         if (people.contains("John")) {
+             System.out.println("John is in the list.");
+         }
+
+         if (people.isEmpty()) {
+             System.out.println("Nothing is in the list.");
+         }
+
+         people.clear();
+
+         /*
+         Linked list. The difference between it and an array list is that elements are stored as
+         nodes that connect its previous node and the next.
+          */
+
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        list.remove(0);
+        list.remove(Integer.valueOf(3));
+        list.clear();
+
     }
 }
