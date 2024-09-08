6dbb9d4d65f1486228098aa085b4424a718d2269
Faizan-Comp <sweeptheblade@gmail.com>
2024-09-08 16:45:36 -0400

    Polymorphism
    Learning polymorphism.

--- Main.java
+++ Main.java
@@ -1,22 +1,14 @@
-import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Animal animal;
-        Scanner s = new Scanner(System.in);
-        System.out.println("Would you like to create a cat object (1) or a Dog object (2)?");
-        int choice = s.nextInt();
-        if (choice == 1) {
-            animal = new Dog();
-            animal.speak();
-        }
-        else if(choice == 2) {
-            animal = new Cat();
-            animal.speak();
-        }
-        else {
-            animal = new Animal();
-            animal.speak();
+        Car car = new Car();
+        Bicycle bicycle = new Bicycle();
+        Boat boat = new Boat();
+
+        Vehicle[] racers = {car, bicycle, boat};
+
+        for(Vehicle x : racers) {
+            x.go();
         }
     }
 }
