3d28bacc098673e82a8b627de07b13f90e334bb0
Faizan-Comp <sweeptheblade@gmail.com>
2024-09-08 16:39:54 -0400

    Intro to Iterators.
    Basic overview of Iterators.

--- Main.java
+++ Main.java
@@ -1,18 +1,42 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Main {
     public static void main(String[] args) {
 
-        MyGenericClass<Integer,Integer> myInt = new MyGenericClass<>(1,2);
-        MyGenericClass<Double,Double> myDouble = new MyGenericClass<>(3.14,1.13);
-        //MyGenericClass<Character,Character> myChar = new MyGenericClass<>('@','$');
-        //MyGenericClass<String,Character> myString = new MyGenericClass<>("Hello",'_');
+        ArrayList<String> foods = new ArrayList<>();
+        foods.add("Apple");
+        foods.add("Banana");
+        foods.add("Bread");
+        foods.add("Cake");
+        foods.add("Pizza");
 
+        /*
+        To iterate through the collection, use a while loop or an enhanced for loop with
+        the condition of has String [Enhanced For] or hasNext() [While Loop].
+         */
+        for (String food : foods) {
+            System.out.println(food);
+        }
 
-        System.out.println(myInt.getValue());
-        System.out.println(myDouble.getValue());
-        //System.out.println(myChar.getValue());
-        //System.out.println(myString.getValue());
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(48);
+        numbers.add(3473);
+        numbers.add(1234);
+        numbers.add(-309);
 
+        Iterator<Integer> it = numbers.iterator();
+
+        while(it.hasNext()) {
+            int i = it.next();
+            if(i > 0) {
+                System.out.println(i);
+            }
+            else if(i < 0) {
+                it.remove();
+            }
+        }
+        System.out.println(numbers);
     }
 }
